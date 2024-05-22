bbff0f3bad8248118c25bd5413838746b523d4b5
Rushan Sitdikov <rsitdikov03@gmail.com>
2024-05-22 20:41:20 +0300

    scroll to the bottom

--- src/components/main/main-block/main-block.tsx
+++ src/components/main/main-block/main-block.tsx
@@ -1,41 +1,35 @@
-import React, { useRef } from "react";
+import React, { useRef } from 'react';  
+import {   
+    Block1,  
+    Block11,  
+    Text,   
+    Sign,  
+    Abutton,  
+    Butlink  
+} from "./main-block.style";  
+  
+export const Block = () => {
+    const endOfPageRef = useRef(null);
 
-import {
-    Block1,
-    Block11,
-    Text,
-    Sign,
-    Abutton,
-    Butlink
-} from "./main-block.style";
-
-const Block = () => {
-    // Создаем реф, который будем связывать с элементом внизу страницы
-    const bottomRef = useRef(null);
-
-    // Функция для обработки клика, которая прокручивает к рефу
     const scrollToBottom = () => {
-        bottomRef.current?.scrollIntoView({
-            behavior: "smooth",
-            block: "end",
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth'
         });
     };
 
     return (
         <>
-            <Block1>
-                <Block11>
-                    <Text>Школа актерского мастерства School Stage</Text>
-                    <Sign>для детей от 7 до 14 лет</Sign>
+            <Block1>  
+                <Block11>  
+                    <Text>Школа актерского мастерства School Stage</Text>  
+                    <Sign>для детей от 7 до 14 лет</Sign>  
                     <Abutton onClick={scrollToBottom}>
                         <Butlink>Оставить заявку</Butlink>
-                    </Abutton>
-                </Block11>
-            </Block1>
-            {/* Добавляем элемент внизу страницы */}
-            <div ref={bottomRef} style={{ height: "1px" }}></div>
+                    </Abutton>  
+                </Block11>  
+            </Block1> 
+            <div ref={endOfPageRef} />
         </>
     );
-}
-
-export default Block;
+};
